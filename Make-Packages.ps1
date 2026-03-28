3e56d6e56ddb7aeb635e79461dee1bbfe3680993
Jonathon Cobb <1852028+jonatron55@users.noreply.github.com>
2026-03-27 23:44:59 -0700

    Fix packaging script

--- Make-Packages.ps1
+++ Make-Packages.ps1
@@ -15,12 +15,96 @@ param(
     [Parameter(HelpMessage = "If present, increment the patch version number.")]
     [switch] $PatchIncrement,
 
+    [Parameter(HelpMessage = "If present, build zip archive.")]
+    [switch] $Zip,
+
+    [Parameter(HelpMessage = "If present, build standalone installer.")]
+    [switch] $Installer,
+
+    [Parameter(HelpMessage = "If present, build MSIX package.")]
+    [switch] $MSIX,
+
     [Parameter(HelpMessage = "The code signing certificate used to sign both the binaries and the installer. If not provided, the package will not be signed. Use the accompanying New-DevSigningCertificate.ps1 script to create a new code signing certificate.")]
     $Certificate = $null,
 
     [Parameter(HelpMessage = "Timestamp server URL to use when signing.")]
     $TimestampServer = "http://timestamp.digicert.com"
 )
+
+# Produces and MSIX app manifest based on a Cargo manifest and a signing certificate.
+function New-AppxManifest($cargo, $certificate) {
+    $id = "$($cargo.package.authors[0].Replace(' ', '')).$($cargo.package.description.Replace(' ', ''))"
+
+
+    $appx = [System.Text.StringBuilder]::new()
+
+    [void] $appx.AppendLine("<?xml version='1.0' encoding='utf-8'?>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("<Package xmlns='http://schemas.microsoft.com/appx/manifest/foundation/windows10'")
+    [void] $appx.AppendLine("         xmlns:uap='http://schemas.microsoft.com/appx/manifest/uap/windows10'")
+    [void] $appx.AppendLine("         xmlns:uap5='http://schemas.microsoft.com/appx/manifest/uap/windows10/5'")
+    [void] $appx.AppendLine("         xmlns:uap10='http://schemas.microsoft.com/appx/manifest/uap/windows10/10'")
+    [void] $appx.AppendLine("         xmlns:desktop4='http://schemas.microsoft.com/appx/manifest/desktop/windows10/4'")
+    [void] $appx.AppendLine("         xmlns:rescap='http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities'")
+    [void] $appx.AppendLine("         IgnorableNamespaces='uap uap5 uap10 desktop4 rescap'>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Identity Name='$id'")
+    [void] $appx.AppendLine("            Version='$($cargo.package.version).0'")
+    [void] $appx.AppendLine("            Publisher='$($certificate.Subject)'")
+    [void] $appx.AppendLine("            ProcessorArchitecture='x64' />")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Properties>")
+    [void] $appx.AppendLine("    <DisplayName>$($cargo.package.description)</DisplayName>")
+    [void] $appx.AppendLine("    <PublisherDisplayName>$($cargo.package.authors[0])</PublisherDisplayName>")
+    [void] $appx.AppendLine("    <Logo>Logo.png</Logo>")
+    [void] $appx.AppendLine("  </Properties>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Resources>")
+    [void] $appx.AppendLine("    <Resource Language='en-us' />")
+    [void] $appx.AppendLine("  </Resources>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Dependencies>")
+    [void] $appx.AppendLine("    <TargetDeviceFamily Name='Windows.Desktop'")
+    [void] $appx.AppendLine("                        MinVersion='10.0.19041.0'")
+    [void] $appx.AppendLine("                        MaxVersionTested='10.0.26100.0' />")
+    [void] $appx.AppendLine("  </Dependencies>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Capabilities>")
+    [void] $appx.AppendLine("    <rescap:Capability Name='runFullTrust' />")
+    [void] $appx.AppendLine("  </Capabilities>")
+    [void] $appx.AppendLine()
+    [void] $appx.AppendLine("  <Applications>")
+
+    foreach ($bin in $cargo.bin) {
+        [void] $appx.AppendLine("    <Application Id='$($bin.name)'")
+        [void] $appx.AppendLine("                 Executable='$($bin.name).exe'")
+        [void] $appx.AppendLine("                 desktop4:Subsystem='console'")
+        [void] $appx.AppendLine("                 desktop4:SupportsMultipleInstances='true'")
+        [void] $appx.AppendLine("                 uap10:Subsystem='console'")
+        [void] $appx.AppendLine("                 uap10:SupportsMultipleInstances='true'")
+        [void] $appx.AppendLine("                 uap10:TrustLevel='mediumIL'")
+        [void] $appx.AppendLine("                 uap10:RuntimeBehavior='win32App'>")
+        [void] $appx.AppendLine("      <uap:VisualElements DisplayName='$($bin.name)'")
+        [void] $appx.AppendLine("                          Description='$($cargo.package.description)'")
+        [void] $appx.AppendLine("                          BackgroundColor='transparent'")
+        [void] $appx.AppendLine("                          Square150x150Logo='Logo.png'")
+        [void] $appx.AppendLine("                          Square44x44Logo='Logo.png' />")
+        [void] $appx.AppendLine("      <Extensions>")
+        [void] $appx.AppendLine("        <uap5:Extension Category='windows.appExecutionAlias'>")
+        [void] $appx.AppendLine("          <uap5:AppExecutionAlias desktop4:Subsystem='console'")
+        [void] $appx.AppendLine("                                  uap10:Subsystem='console'>")
+        [void] $appx.AppendLine("            <uap5:ExecutionAlias Alias='$($bin.name).exe' />")
+        [void] $appx.AppendLine("          </uap5:AppExecutionAlias>")
+        [void] $appx.AppendLine("        </uap5:Extension>")
+        [void] $appx.AppendLine("      </Extensions>")
+        [void] $appx.AppendLine("    </Application>")
+    }
+
+    [void] $appx.AppendLine("  </Applications>")
+    [void] $appx.AppendLine("</Package>")
+
+    return $appx.ToString()
+}
 
 $ErrorActionPreference = "Stop"
 try {
@@ -31,6 +115,7 @@ catch {
     exit 1
 }
 
+$config = if ($Release) { "release" } else { "debug" }
 
 # Read metadata from Cargo.toml.
 $cargo = Get-Content -Path "Cargo.toml"
@@ -64,7 +149,6 @@ elseif ($PatchIncrement) {
 }
 
 $version = $parts -join '.'
-$config = if ($Release) { "release" } else { "debug" }
 
 Write-Host -ForegroundColor Cyan "====== Build $packageName version $version ======"
 Write-Host
@@ -73,8 +157,8 @@ Write-Host -ForegroundColor Blue "  Display Name: $displayName"
 Write-Host -ForegroundColor Blue "  Author: $author"
 
 # Update version in Cargo.toml.
-$cargo = $cargo -replace "^version = ""(.*)""", "version = ""$version"""
-$cargo | Set-Content -Path "Cargo.toml"
+# $cargo = $cargo -replace "^version = ""(.*)""", "version = ""$version"""
+# $cargo | Set-Content -Path "Cargo.toml"
 
 # Clean build artifacts.
 if ($Clean) {
@@ -89,13 +173,17 @@ else {
     cargo.exe build
 }
 
+if (-not (Test-Path ".\dist")) {
+    New-Item -ItemType Directory -Path "dist"
+}
+
 # Export certificate, if provided.
 if ($Certificate) {
     Write-Host
-    Write-Host -ForegroundColor Cyan "====== Exporting $(certificate.Thumbprint) ======"
+    Write-Host -ForegroundColor Cyan "====== Exporting $($certificate.Thumbprint) ======"
     Write-Host
 
-    Export-Certificate -Cert $Certificate -FilePath "dist\$(certificate.Thumbprint).cer" -Type CERT -Force
+    Export-Certificate -Cert $Certificate -FilePath "dist\$($certificate.Thumbprint).cer" -Type CERT -Force
 }
 
 # Sign the binary, if a certificate is provided.
@@ -120,158 +208,109 @@ else {
 }
 
 # Create a plain zip archive.
-Write-Host
-Write-Host -ForegroundColor Cyan "====== Creating archive ======"
-Write-Host
-
-Compress-Archive `
-    -Path "target\$config\*.exe", "License.txt" `
-    -DestinationPath "$packageFilename.zip" `
-    -CompressionLevel Optimal `
-    -Force
-
-if ((Test-Path "$packageFilename.zip") -and $Certificate) {
+if ($Zip) {
     Write-Host
-    Write-Host -ForegroundColor Cyan "====== Signing $packageFilename.zip ======"
+    Write-Host -ForegroundColor Cyan "====== Creating archive ======"
     Write-Host
 
-    Set-AuthenticodeSignature `
-        -FilePath "$packageFilename.zip" `
-        -Certificate $Certificate `
-        -TimestampServer $TimestampServer
+    if (Test-Path "dist\$packageFilename.zip") {
+        Remove-Item "dist\$packageFilename.zip" -Force
+    }
+
+    Compress-Archive `
+        -Path "target\$config\*.exe", "License.txt" `
+        -DestinationPath "dist\$packageFilename.zip" `
+        -CompressionLevel Optimal `
+        -Force
 }
 
 # Build the standalone installer.
-Write-Host
-Write-Host -ForegroundColor Cyan "====== Building standalone installer ======"
-Write-Host
-
-if (-not (Test-Path "dist")) {
-    New-Item -ItemType Directory -Path "dist"
-}
-
-makensis.exe `
-    -DBIN_NAME="$binName" `
-    -DPKG_NAME="$packageName" `
-    -DAUTHOR="$author" `
-    -DVERSION="$version" `
-    -DCONFIG="$config" `
-    -DROOT="$(Get-Location)" `
-    -X"Name `"$displayName`"" `
-    "src\package\installer.nsi"
-
-
-# Sign the installer, if a certificate is provided.
-if ($LASTEXITCODE -eq 0 -and $Certificate) {
-    Write-Error "Build failed."
-    $targetBin = "dist\$packageFilename.exe"
+if ($Installer) {
     Write-Host
-    Write-Host -ForegroundColor Cyan "====== Signing $targetBin ======"
+    Write-Host -ForegroundColor Cyan "====== Building standalone installer ======"
     Write-Host
 
-    Set-AuthenticodeSignature `
-        -FilePath $targetBin `
-        -Certificate $Certificate `
-        -TimestampServer $TimestampServer
+    if (-not (Test-Path "dist")) {
+        New-Item -ItemType Directory -Path "dist"
+    }
+
+    makensis `
+        -DBIN_NAME="$binName" `
+        -DPKG_NAME="$packageName" `
+        -DAUTHOR="$author" `
+        -DVERSION="$version" `
+        -DCONFIG="$config" `
+        -DROOT="$(Get-Location)" `
+        -X"Name `"$displayName`"" `
+        "src\package\installer.nsi"
+
+
+    # Sign the installer, if a certificate is provided.
+    if ($LASTEXITCODE -eq 0 -and $Certificate) {
+        $targetBin = "dist\$packageFilename.exe"
+        Write-Host
+        Write-Host -ForegroundColor Cyan "====== Signing $targetBin ======"
+        Write-Host
+
+        Set-AuthenticodeSignature `
+            -FilePath $targetBin `
+            -Certificate $Certificate `
+            -TimestampServer $TimestampServer
+    }
 }
 
 # Build the MSIX package.
-Write-Host
-Write-Host -ForegroundColor Cyan "====== Building MSIX package ======"
-Write-Host
-
-$manifest = New-AppxManifest -cargo $cargo -certificate $Certificate
-$manifest | Set-Content -Path "target\package\AppxManifest.xml" -Encoding utf8
-
-$mapping = [System.Text.StringBuilder]::new()
-$mapping.AppendLine("[Files]")
-$mapping.AppendLine("`"target\package\AppxManifest.xml`"`t`"AppxManifest.xml`"")
-foreach ($bin in $cargo.bin) {
-    $mapping.AppendLine("`"target\$config\$($bin.name).exe`t`"$($bin.name).exe`"")
-}
-$mapping.AppendLine("`"License.txt`t`"License.txt`"")
-$mapping.ToString() | Set-Content -Path "target\package\mapping.txt"
-
-MakeAppx.exe pack `
-    -f "target\package\mapping.txt" `
-    -p "dist\$packageFilename.msix" `
-    -o
-
-if ($LASTEXITCODE -eq 0 -and $Certificate) {
+if ($MSIX) {
     Write-Host
-    Write-Host -ForegroundColor Cyan "====== Signing MSIX package ======"
+    Write-Host -ForegroundColor Cyan "====== Building MSIX package ======"
     Write-Host
 
-    Set-AuthenticodeSignature `
-        -FilePath "dist\$packageFilename.msix" `
-        -Certificate $Certificate `
-        -TimestampServer $TimestampServer
-}
-
-# Produces and MSIX app manifest based on a Cargo manifest and a signing certificate.
-function New-AppxManifest($cargo, $certificate) {
-    $id = "$($cargo.package.authors[0].Replace(' ', '')).$($cargo.description.Replace(' ', ''))"
-
-
-    $appx = [System.Text.StringBuilder]::new()
-
-    $appx.AppendLine("<?xml version='1.0' encoding='utf-8'?>")
-    $appx.AppendLine()
-    $appx.AppendLine("<Package xmlns='http://schemas.microsoft.com/appx/2010/manifest'")
-    $appx.AppendLine("         xmlns:uap='http://schemas.microsoft.com/appx/manifest/uap/windows10'")
-    $appx.AppendLine("         xmlns:uap5='http://schemas.microsoft.com/appx/manifest/uap/windows10/5'")
-    $appx.AppendLine("         xmlns:uap10='http://schemas.microsoft.com/appx/manifest/uap/windows10/10'")
-    $appx.AppendLine("         xmlns:desktop4='http://schemas.microsoft.com/appx/manifest/desktop/windows10/4'")
-    $appx.AppendLine("         xmlns:rescap='http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities'")
-    $appx.AppendLine("         ignorableNamespaces='uap'>")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Identity Name='$id'")
-    $appx.AppendLine("            Version='$($cargo.package.version).0'")
-    $appx.AppendLine("            Publisher='$($certificate.Subject)'")
-    $appx.AppendLine("            ProcessorArchitecture='x64' />")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Properties>")
-    $appx.AppendLine("    <DisplayName>$($cargo.package.description)</DisplayName>")
-    $appx.AppendLine("    <PublisherDisplayName>$($cargo.package.authors[0])</PublisherDisplayName>")
-    $appx.AppendLine("    <Logo></Logo>")
-    $appx.AppendLine("  </Properties>")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Resources>")
-    $appx.AppendLine("    <Resource Language='en-us' />")
-    $appx.AppendLine("  </Resources>")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Dependencies>")
-    $appx.AppendLine("    <TargetDeviceFamily Name='Windows.Desktop'")
-    $appx.AppendLine("                        MinVersion='10.0.19041.0'")
-    $appx.AppendLine("                        MaxVersionTested='10.0.26100.0' />")
-    $appx.AppendLine("  </Dependencies>")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Capabilities>")
-    $appx.AppendLine("    <rescap:Capability Name='runFullTrust' />")
-    $appx.AppendLine("  </Capabilities>")
-    $appx.AppendLine()
-    $appx.AppendLine("  <Applications>")
-
-    foreach ($bin in $cargo.bin) {
-        $appx.AppendLine("    <Application Id='$($bin.name)'")
-        $appx.AppendLine("                 Executable='$($bin.name).exe'")
-        $appx.AppendLine("                 desktop4:Subsystem='console'")
-        $appx.AppendLine("                 uap10:Subsystem='console'")
-        $appx.AppendLine("                 uap10:TrustLevel='mediumIL'")
-        $appx.AppendLine("                 uap10:RuntimeBehavior='win32App'>")
-        $appx.AppendLine("      <Extensions>")
-        $appx.AppendLine("        <uap5:Extension>")
-        $appx.AppendLine("          <uap5:AppExecutionAlias desktop4:Subsystem='console'")
-        $appx.AppendLine("                                  uap10:Subsystem='console'>")
-        $appx.AppendLine("            <uap5:ExecutionAlias Alias='$($bin.name)' />")
-        $appx.AppendLine("          </uap5:AppExecutionAlias>")
-        $appx.AppendLine("        </uap5:Extension>")
-        $appx.AppendLine("      </Extensions>")
-        $appx.AppendLine("    </Application>")
+    if (-not (Test-Path "target\package")) {
+        New-Item -ItemType Directory -Path "target\package"
     }
 
-    $appx.AppendLine("  </Applications>")
-    $appx.AppendLine("</Package>")
+    Copy-Item -Path "assets\doodle.png" -Destination "target\package\Logo.png" -Force
+    $manifest = New-AppxManifest -cargo $cargo -certificate $Certificate
+    $manifest | Set-Content -Path "target\package\AppxManifest.xml" -Encoding utf8
 
-    return $appx.ToString()
+    $mapping = [System.Text.StringBuilder]::new()
+    $mapping.AppendLine("[Files]")
+    $mapping.AppendLine("`"target\package\AppxManifest.xml`"`t`"AppxManifest.xml`"")
+    $mapping.AppendLine("`"target\package\Logo.png`"`t`"Logo.png`"")
+    foreach ($bin in $cargo.bin) {
+        $mapping.AppendLine("`"target\$config\$($bin.name).exe`"`t`"$($bin.name).exe`"")
+    }
+    $mapping.AppendLine("`"License.txt`"`t`"License.txt`"")
+    $mapping.ToString() | Set-Content -Path "target\package\mapping.txt"
+
+    MakeAppx.exe pack `
+        -f "target\package\mapping.txt" `
+        -p "dist\$packageFilename.msix" `
+        -o
+
+    if ($LASTEXITCODE -eq 0 -and $Certificate) {
+        $targetMsix = "dist\$packageFilename.msix"
+        Write-Host
+        Write-Host -ForegroundColor Cyan "====== Signing MSIX package ======"
+        Write-Host
+
+        $signTool = Get-Command -Name "signtool.exe" -ErrorAction SilentlyContinue
+        if (-not $signTool) {
+            Write-Error "signtool.exe was not found in PATH. Install Windows SDK and ensure signtool.exe is available."
+            exit 1
+        }
+
+        & $signTool.Source sign `
+            /fd SHA256 `
+            /sha1 $Certificate.Thumbprint `
+            /tr $TimestampServer `
+            /td SHA256 `
+            /v `
+            $targetMsix
+
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "SignTool failed to sign $targetMsix"
+            exit $LASTEXITCODE
+        }
+    }
 }
--- src/package/installer.nsi
+++ src/package/installer.nsi
@@ -1,110 +1,110 @@
 # Copyright (c) 2025 Jonathon Burnham Cobb
 # Licensed under the MIT-0 license.
 
-Unicode True
-SetCompressor LZMA
+Unicode true
+SetCompressor lzma
 
-!Define COPYRIGHT "Copyright (c) 2025 ${AUTHOR}."
+!define COPYRIGHT "Copyright (c) 2025 ${AUTHOR}."
 
-!Include "LogicLib.nsh"
+!include "LogicLib.nsh"
 
-!Define UNINSTALLER_NAME "$InstDir\uninstall.exe"
+!define UNINSTALLER_NAME "$InstDir\uninstall.exe"
 
-#
+# 
 # LANGUAGE STRINGS
-#
+# 
 
 LoadLanguageFile "${NSISDIR}\Contrib\Language files\English.nlf"
 LangString VersionText ${LANG_ENGLISH} "Version ${VERSION}"
-SetFont /Lang=${LANG_ENGLISH} "Segoe UI" 9
+SetFont /LANG=${LANG_ENGLISH} "Segoe UI" 9
 
-#
+# 
 # INSTALLER ATTRIBUTES
-#
+# 
 
 InstallDir "$LOCALAPPDATA\Programs\${PKG_NAME}"
-InstallDirRegkey HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir"
-RequestExecutionLevel User
-XPStyle On
-InstallColors /Windows
+InstallDirRegKey HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir"
+RequestExecutionLevel user
+XPStyle on
+InstallColors /windows
 InstProgressFlags smooth
-Icon "${NSISDIR}\Contrib\Graphics\Icons\orange-install.ico"
+Icon "${ROOT}\assets\doodle.ico"
 UninstallIcon "${NSISDIR}\Contrib\Graphics\Icons\orange-uninstall.ico"
 BrandingText $(VersionText)
 ManifestSupportedOS Win10
-ManifestDPIAware True
-WindowIcon Off
-LicenseData "${ROOT}\License.txt"
+ManifestDPIAware true
+; WindowIcon off
+LicenseData "${ROOT}\src\package\License-nowrap.txt"
 OutFile "${ROOT}\dist\${PKG_NAME}-${CONFIG}-${VERSION}.exe"
 
 VIProductVersion "${VERSION}.0"
 VIFileVersion "${VERSION}.0"
 
-VIAddVersionKey /Lang=${LANG_ENGLISH} "ProductName" "${PKG_NAME}"
-VIAddVersionKey /Lang=${LANG_ENGLISH} "CompanyName" "${AUTHOR}"
-VIAddVersionKey /Lang=${LANG_ENGLISH} "LegalCopyright" "${COPYRIGHT}"
-VIAddVersionKey /Lang=${LANG_ENGLISH} "FileDescription" "${PKG_NAME} Installer"
-VIAddVersionKey /Lang=${LANG_ENGLISH} "ProductVersion" "${VERSION}.0"
-VIAddVersionKey /Lang=${LANG_ENGLISH} "FileVersion" "${VERSION}.0"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductName" "${PKG_NAME}"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "CompanyName" "${AUTHOR}"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "LegalCopyright" "${COPYRIGHT}"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "FileDescription" "${PKG_NAME} Installer"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "ProductVersion" "${VERSION}.0"
+VIAddVersionKey /LANG=${LANG_ENGLISH} "FileVersion" "${VERSION}.0"
 
-Page License
-Page Directory
-Page InstFiles
-UninstPage UninstConfirm
-UninstPage InstFiles
+Page license
+Page directory
+Page instfiles
+UninstPage uninstConfirm
+UninstPage instfiles
 
-#
+# 
 # INSTALLER SECTIONS
-#
+# 
 
 Section
-    # Clean, if needed
-    ${If} ${FileExists} "$INSTDIR\${BIN_NAME}"
-        RMDir /R $INSTDIR
-    ${EndIf}
+	# Clean, if needed
+	${If} ${FileExists} "$INSTDIR\${BIN_NAME}"
+		RMDir /r $INSTDIR
+	${EndIf}
 
-    # Install Binaries
-    SetOverwrite On
-    CreateDirectory $INSTDIR
-    SetOutPath $INSTDIR
+	# Install Binaries
+	SetOverwrite on
+	CreateDirectory $INSTDIR
+	SetOutPath $INSTDIR
 
-    File "${ROOT}\target\${CONFIG}\*.exe"
-    File "${ROOT}\LICENSE.txt"
+	File "${ROOT}\target\${CONFIG}\*.exe"
+	File "${ROOT}\LICENSE.txt"
 
-    # Update PATH
-    EnVar::SetHKCU
-    EnVar::AddValue "PATH" "$INSTDIR"
+	# Update PATH
+	EnVar::SetHKCU
+	EnVar::AddValue "PATH" "$INSTDIR"
 
-    # Register and create uninstaller
-    WriteRegStr HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir" $INSTDIR
-    WriteRegStr HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "Version" ${VERSION}
-    WriteRegStr HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "DisplayName" "$(^Name)"
-    WriteRegStr HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "UninstallString" '"${UNINSTALLER_NAME}"'
-    WriteUninstaller ${UNINSTALLER_NAME}
+	# Register and create uninstaller
+	WriteRegStr HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir" $INSTDIR
+	WriteRegStr HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "Version" ${VERSION}
+	WriteRegStr HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "DisplayName" "$(^Name)"
+	WriteRegStr HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "UninstallString" '"${UNINSTALLER_NAME}"'
+	WriteUninstaller ${UNINSTALLER_NAME}
 SectionEnd
 
-#
+# 
 # UNINSTALLER SECTIONS
-#
+# 
 
 Section "Uninstall"
-    # Remove files
-    Delete "${UNINSTALLER_NAME}"
+	# Remove files
+	Delete "${UNINSTALLER_NAME}"
 
-    RMDir /R $INSTDIR
-    RMDir /R "$LOCALAPPDATA\${AUTHOR}\${PKG_NAME}"
+	RMDir /r $INSTDIR
+	RMDir /r "$LOCALAPPDATA\${AUTHOR}\${PKG_NAME}"
 
-    # Update PATH
-    EnVar::SetHKCU
-    EnVar::DeleteValue "PATH" "$INSTDIR" 
+	# Update PATH
+	EnVar::SetHKCU
+	EnVar::DeleteValue "PATH" "$INSTDIR"
 
-    # Remove uninstaller registration
-    DeleteRegValue HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir"
-    DeleteRegValue HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "Version"
-    DeleteRegKey HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup"
-    DeleteRegKey HKCU "Software\${AUTHOR}\${PKG_NAME}"
+	# Remove uninstaller registration
+	DeleteRegValue HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "InstallDir"
+	DeleteRegValue HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup" "Version"
+	DeleteRegKey HKCU "Software\${AUTHOR}\${PKG_NAME}\Setup"
+	DeleteRegKey HKCU "Software\${AUTHOR}\${PKG_NAME}"
 
-    DeleteRegValue HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "UninstallString"
-    DeleteRegValue HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "DisplayName"
-    DeleteRegKey HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}"
+	DeleteRegValue HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "UninstallString"
+	DeleteRegValue HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}" "DisplayName"
+	DeleteRegKey HKCU "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PKG_NAME}"
 SectionEnd
